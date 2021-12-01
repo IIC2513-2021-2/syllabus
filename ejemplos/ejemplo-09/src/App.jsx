9fa63265fec5b35ec8d530a84a8c103cacc6d51c
Sebastián Vicencio <sivicencio@gmail.com>
2021-12-01 16:33:49 +0100

    update example 09 with effects

--- ejemplos/ejemplo-09/src/App.jsx
+++ ejemplos/ejemplo-09/src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [mounted, setMounted] = useState(true);
+
+  const toggleMounted = () => {
+    setMounted((prevMounted) => !prevMounted);
+  };
+
   return (
     <main className="centered">
-      <TodoList />
+      <button type="button" onClick={toggleMounted}>
+        {mounted ? 'Unmount' : 'Mount'}
+      </button>
+      {mounted ? <TodoList /> : null}
     </main>
   );
 }
